1f9c4fbc38a6b05829548b0adc6ea6b0dce798be
Selami <bagcieselami@hotmail.fr>
2019-02-11 13:09:55 +0100

    classe affichant un message selon le jeu et le mode choisi en éxécutant le choix

--- src/com/jeu/Choix.java
+++ src/com/jeu/Choix.java
@@ -9,23 +9,29 @@ public class Choix {
 		String choixDuel = "Mode duel";
 
 		if (jeu == '1' && modeJeu == '1') {
-			System.out.println("Vous avez choisi " + choixChallenger);
+			System.out.println("Vous avez choisi le " + choixChallenger);
 			System.out.println("Vous devez trouver la combinaison secrète de l'ordinateur.");
+			ChallengerRecherche.ChallengerR();
 		} else if (jeu == '1' && modeJeu == '2') {
-			System.out.println("Vous avez choisi " + choixDefenseur);
+			System.out.println("Vous avez choisi le " + choixDefenseur);
 			System.out.println("C'est à l'ordinateur de trouver votre combinaison secrète.");
+			DefenseurRecherche.DefenseurR();
 		} else if (jeu == '1' && modeJeu == '3') {
-			System.out.println("Vous avez choisi " + choixDuel);
+			System.out.println("Vous avez choisi le " + choixDuel);
 			System.out.println("L'ordinateur et vous jouez tour à tour, le premier à trouver la combinaison secrète de l'autre a gagné.");
+			DuelRecherche.DuelR();
 		} else if (jeu == '2' && modeJeu == '1') {
-			System.out.println("Vous avez choisi " + choixChallenger);
+			System.out.println("Vous avez choisi le " + choixChallenger);
 			System.out.println("Vous devez trouver la combinaison secrète de l'ordinateur.");
+			ChallengerMastermind.ChallengerM();
 		} else if (jeu == '2' && modeJeu == '2') {
-			System.out.println("Vous avez choisi " + choixDefenseur);
+			System.out.println("Vous avez choisi le " + choixDefenseur);
 			System.out.println("C'est à l'ordinateur de trouver votre combinaison secrète.");
+			DefenseurMastermind.DefenseurM();
 		} else if (jeu == '2' && modeJeu == '3') {
-			System.out.println("Vous avez choisi " + choixDuel);
+			System.out.println("Vous avez choisi le " + choixDuel);
 			System.out.println("L'ordinateur et vous jouez tour à tour, le premier à trouver la combinaison secrète de l'autre a gagné.");
+			DuelMastermind.DuelM();
 		} 
 	}
 }

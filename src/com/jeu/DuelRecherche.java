aa6f2000f6e0171f5e153e5fc792039a9c97a9b2
Selami <bagcieselami@hotmail.fr>
2019-02-11 10:42:06 +0100

    Code du mode Duel du jeu Recherche

---
+++ src/com/jeu/DuelRecherche.java
@@ -0,0 +1,81 @@
+package com.jeu;
+
+import java.util.ArrayList;
+
+public class DuelRecherche {
+
+	public static void DuelR() {
+
+		Humain humain = new Humain();
+		Ordi ordi = new Ordi();
+		int nbreEssaiMax = 10;
+		ArrayList<Integer> propositionOrdi = null;
+		String resultatJoueur = null;
+		String resultatOrdi = null;
+
+		ArrayList<Integer> codeJoueurCherche = ordi.codeAleatoire();
+
+		System.out.println("\nSaisir une combinaison entre 0 et 6 à 4 chiffres à faire deviner à l'ordinateur. ");
+		ArrayList<Integer> codeOrdiCherche = humain.codeJoueur();
+
+		System.out.print("Votre combinaison : ");
+		for (int i = 0; i < codeOrdiCherche.size(); i++) {
+			System.out.print(codeOrdiCherche.get(i));
+		}
+		System.out.println();
+
+		while (nbreEssaiMax > 0) {
+
+			System.out.println("\nSaisir une combinaison entre 0 et 6 à 4 chiffres");
+			ArrayList<Integer> propositionJoueur = humain.codeJoueur();
+
+			System.out.print("\nProposition du joueur : ");
+			for (int i = 0; i < codeJoueurCherche.size(); i++) {
+				System.out.print(propositionJoueur.get(i));
+			}
+			System.out.print("\nRéponse : ");
+
+			// comparer propositionJoueur avec codeJoueurCherche pour donner un resultat
+			resultatJoueur = Utilitaire.getResultatRechercher(codeJoueurCherche, propositionJoueur);
+					
+			System.out.println(resultatJoueur);
+			
+			propositionOrdi = ordi.codeAleatoireRecherche(propositionOrdi, resultatOrdi);
+			
+			//Afficher la proposition de l'ordinateur
+			System.out.print("\nProposition de l'ordinateur : ");
+			for (int i = 0; i < codeOrdiCherche.size(); i++) {
+				System.out.print(propositionOrdi.get(i));
+			}
+			System.out.print("\nRéponse : ");
+			
+			// comparer propositionOrdi avec codeOrdiCherche pour donner un resultat (+, - ou =)
+			resultatOrdi = Utilitaire.getResultatRechercher(codeOrdiCherche, propositionOrdi);
+			System.out.println(resultatOrdi);
+			
+			//le joueur qui trouve la combinaison de l'autre alors le jeu l'affiche
+			if (resultatOrdi.equals("====")) {
+				System.out.println("Bravo ordinateur, vous avez gagné.");
+				nbreEssaiMax = 0;
+			} else if (resultatJoueur.equals("====")) {
+				System.out.println("Bravo joueur, vous avez gagné.");
+				nbreEssaiMax = 0;
+			}
+			nbreEssaiMax--;
+			
+			//Si aucun des 2 joueurs trouve la combinaison de l'autre avant le nombre d'essai maximum le jeu affiche les 2 combinaison en indiquant perdu
+			if(nbreEssaiMax == 0) {
+				System.out.println("\nVous avez tout les 2 perdu.");
+				System.out.println("La combinaison du joueur etait : ");
+				for (int i = 0 ; i < codeOrdiCherche.size() ; i++) {
+					System.out.print(codeOrdiCherche.get(i));
+				}
+				System.out.println(".");
+				System.out.println("La combinaison de l'ordinateur était : ");
+				for (int i = 0 ; i < codeJoueurCherche.size() ; i++) {
+					System.out.print(codeJoueurCherche.get(i));
+				}
+			}
+		}
+	}	
+}

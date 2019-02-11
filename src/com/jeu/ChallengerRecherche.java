521b9c4287234682b5d57670d4975b0afae2bcd7
BAGCI <bagciselami@hotmail.fr>
2019-02-12 00:41:18 +0100

    supression intanciation Humain et Ordi

--- src/com/jeu/ChallengerRecherche.java
+++ src/com/jeu/ChallengerRecherche.java
@@ -5,25 +5,22 @@ import java.util.ArrayList;
 public class ChallengerRecherche {
 	
 	public static void ChallengerR(){
-				
-		Humain humain = new Humain();
-		Ordi ordi = new Ordi();
 		
-		// générer la combinaison aléatoire 
-		ArrayList<Integer> codeAleatoire = ordi.codeAleatoire();
+		// generer la combinaison aleatoire 
+		ArrayList<Integer> codeAleatoire = Ordi.codeAleatoire();
 		
 		int nbreEssais = 10;
 				
 		while (nbreEssais > 0) {
 
-			System.out.println("\nSaisir une combinaison à 4 chiffres entre 0 et 6");
-			ArrayList<Integer> proposition = humain.codeJoueur();
+			System.out.println("\nSaisir une combinaison a 4 chiffres entre 0 et 6");
+			ArrayList<Integer> proposition = Humain.codeJoueur();
 
 			System.out.print("Proposition : ");
 			for (int i = 0; i < codeAleatoire.size() ; i++) {
 				System.out.print(proposition.get(i));
 			}
-			System.out.print("\nRéponse : ");
+			System.out.print("\nReponse : ");
 
 			String resultat = Utilitaire.getResultatRechercher(codeAleatoire, proposition);
 					
@@ -31,14 +28,14 @@ public class ChallengerRecherche {
 			
 			// Afficher la victoire si la combinaison est ====
 			if (resultat.equals("====")) {
-				System.out.println("Bravo, vous avez gagné.");
+				System.out.println("Bravo, vous avez gagne.");
 				nbreEssais = 0;
 			}
 			nbreEssais--;
 			
 			//Afficher la combinaison avec les bon resultat
 			if (nbreEssais == 0) {
-				System.out.print("Perdu, la combinaison de l'ordinateur était : ");
+				System.out.print("Perdu, la combinaison de l'ordinateur etait : ");
 				for (int i = 0; i < codeAleatoire.size(); i++) {
 					System.out.print(codeAleatoire.get(i));
 				}

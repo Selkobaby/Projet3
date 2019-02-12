067f66f3f5ffe7327c0980f16870415c2792d3c1
h motors <hmotors@imac-de-h.home>
2019-02-12 18:01:00 +0100

    correction orthographe

--- src/com/jeu/Choix.java
+++ src/com/jeu/Choix.java
@@ -5,32 +5,32 @@ public class Choix {
 	public static void getMode(char jeu, char modeJeu) {
 		
 		String choixChallenger = "Mode challenger";
-		String choixDefenseur = "Mode défenseur";
+		String choixDefenseur = "Mode defenseur";
 		String choixDuel = "Mode duel";
 
 		if (jeu == '1' && modeJeu == '1') {
 			System.out.println("Vous avez choisi le " + choixChallenger);
-			System.out.println("Vous devez trouver la combinaison secrète de l'ordinateur.");
+			System.out.println("Vous devez trouver la combinaison secrete de l'ordinateur.");
 			ChallengerRecherche.ChallengerR();
 		} else if (jeu == '1' && modeJeu == '2') {
 			System.out.println("Vous avez choisi le " + choixDefenseur);
-			System.out.println("C'est à l'ordinateur de trouver votre combinaison secrète.");
+			System.out.println("C'est a l'ordinateur de trouver votre combinaison secrete.");
 			DefenseurRecherche.DefenseurR();
 		} else if (jeu == '1' && modeJeu == '3') {
 			System.out.println("Vous avez choisi le " + choixDuel);
-			System.out.println("L'ordinateur et vous jouez tour à tour, le premier à trouver la combinaison secrète de l'autre a gagné.");
+			System.out.println("L'ordinateur et vous jouez tour a tour, le premier a trouver la combinaison secrete de l'autre a gagne.");
 			DuelRecherche.DuelR();
 		} else if (jeu == '2' && modeJeu == '1') {
 			System.out.println("Vous avez choisi le " + choixChallenger);
-			System.out.println("Vous devez trouver la combinaison secrète de l'ordinateur.");
+			System.out.println("Vous devez trouver la combinaison secrete de l'ordinateur.");
 			ChallengerMastermind.ChallengerM();
 		} else if (jeu == '2' && modeJeu == '2') {
 			System.out.println("Vous avez choisi le " + choixDefenseur);
-			System.out.println("C'est à l'ordinateur de trouver votre combinaison secrète.");
+			System.out.println("C'est a l'ordinateur de trouver votre combinaison secrete.");
 			DefenseurMastermind.DefenseurM();
 		} else if (jeu == '2' && modeJeu == '3') {
 			System.out.println("Vous avez choisi le " + choixDuel);
-			System.out.println("L'ordinateur et vous jouez tour à tour, le premier à trouver la combinaison secrète de l'autre a gagné.");
+			System.out.println("L'ordinateur et vous jouez tour a tour, le premier a trouver la combinaison secrete de l'autre a gagne.");
 			DuelMastermind.DuelM();
 		} 
 	}

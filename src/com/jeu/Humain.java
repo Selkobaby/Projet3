b98f5772018ab672fa7dfcc3d06952be48cc3d78
h motors <hmotors@imac-de-h.home>
2019-02-12 18:03:41 +0100

    correction orthographe

--- src/com/jeu/Humain.java
+++ src/com/jeu/Humain.java
@@ -1,30 +1,31 @@
 package com.jeu;
 
 import java.util.ArrayList;
+
 import java.util.Scanner;
 
 public class Humain {
 	
-	Scanner sc = new Scanner (System.in);
-	
 	/*
-	 * codeJoueur permet de récupérer le code à  4 chiffres que va saisir le Joueur
-	 * pour le mode Mastermind
+	 * codeJoueur permet de recuperer le code e  4 chiffres que va saisir le Joueur
 	 */
-	public ArrayList<Integer> codeJoueur() {
+	public static ArrayList<Integer> codeJoueur() {
 
+		int nbreChiffresCombi = 4;
+		@SuppressWarnings("resource")
+		Scanner sc = new Scanner (System.in);
 		ArrayList<Integer> code = new ArrayList<Integer>();
 		String str = null;
 		do {
 			str = sc.nextLine();
-			//Converti l'objet String en un tableau de caractère
+			//Converti l'objet String en un tableau de caractere
 			char[] tab = str.toCharArray();
 			code.clear();
 			for (int i = 0; i < str.length(); i++) {
 					//Converti le tableau de caractere en un nombre entier
 					code.add(Integer.parseInt(Character.toString(tab[i])));
 			}
-		} while (str.length() != code.size());
+		} while (str.length() != nbreChiffresCombi);
 		return code;
 	}
 }

905befb54a5e7fc19ce6f03c9d246c9cea1a87b0
Selami <bagcieselami@hotmail.fr>
2019-02-13 12:37:17 +0100

    Changer nom codeAleatoir

--- src/com/jeu/Test.java
+++ src/com/jeu/Test.java
@@ -6,21 +6,21 @@ class Test {
 
 	@org.junit.jupiter.api.Test
 	void testNbreChiffresCombiOrdi() {
-		assertEquals(4, Ordi.codeAleatoire().size());
+		assertEquals(4, Ordi.codeAleatoireMastermind().size());
 	}
 	
 	@org.junit.jupiter.api.Test
 	void testValeurCombiMax() {
 		int[] tab = new int [4];
-		for(int i = 0 ; i < Ordi.codeAleatoire().size(); i++) {
-			tab[i] = Ordi.codeAleatoire().get(i);
+		for(int i = 0 ; i < Ordi.codeAleatoireMastermind().size(); i++) {
+			tab[i] = Ordi.codeAleatoireMastermind().get(i);
 		}
 		assertTrue(tab[0] <= 6 && tab[1] <= 6 && tab[2] <= 6 && tab[3] <= 6);
 	}
 	
 	@org.junit.jupiter.api.Test
 	void testGagneMastermind() {
-		int bienPlace = Utilitaire.getBienPlace(Ordi.codeAleatoire(), Humain.codeJoueur());
+		int bienPlace = Utilitaire.getBienPlace(Ordi.codeAleatoireMastermind(), Humain.codeJoueur());
 		assertTrue(bienPlace == 4);
 	}
 	

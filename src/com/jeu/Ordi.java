597dc26ee25cad80a08dbf17b68c0ecf5bda8845
Selami <bagcieselami@hotmail.fr>
2019-02-13 12:31:39 +0100

    ajout code aleatoirR et modification c ode aleatoirRecherche pour incrementer ou decrementer a chaque tour

--- src/com/jeu/Ordi.java
+++ src/com/jeu/Ordi.java
@@ -8,7 +8,7 @@ public class Ordi {
 	/*
 	 * codeAleatoire permet de generer un code aleatoire entre 0 et 6
 	 */
-	public static ArrayList<Integer> codeAleatoire() {
+	public static ArrayList<Integer> codeAleatoireMastermind() {
 		
 		Random r = new Random();
 						
@@ -24,10 +24,30 @@ public class Ordi {
 		}
 		return code;
 	}
+	
+	/*
+	 * codeAleatoire permet de generer un code aleatoire entre 0 et 9
+	 */
+	public static ArrayList<Integer> codeAleatoireR() {
+		
+		Random r = new Random();
+						
+		int nbreChiffresCombi = 4;
+						
+		ArrayList<Integer> code = new ArrayList<Integer>();
+		for (int i = 0; i < nbreChiffresCombi; i++) {
+			int nombre;
+			do {
+				nombre = r.nextInt(10);
+			} while (code.contains(nombre));
+			code.add(nombre);
+		}
+		return code;
+	}
 
 	public static ArrayList<Integer> codeAleatoireRecherche(ArrayList<Integer> codePrecedent, String resultatOrdi) {
 		if(codePrecedent == null && resultatOrdi == null) {
-			return codeAleatoire();
+			return codeAleatoireR();
 		}
 		ArrayList<Integer> code = new ArrayList<Integer>();
 		
@@ -40,17 +60,14 @@ public class Ordi {
 			if (tabResultatOrdi[i] == '=') {
 				code.add(codePrecedent.get(i));
 			} else if (tabResultatOrdi[i] == '+') {
-				// Entre codePrecedent.get(i) + 1 et 0
-				int nombre;
-				int min = codePrecedent.get(i) + 1;
-				int max = 10;
-				nombre = rd.nextInt(max - min) + min;
-				code.add(nombre);
+				//ajout 1 au code precedent 
+				int nombre = codePrecedent.get(i);
+				nombre++;
+				code.add(nombre++);
 			} else if (tabResultatOrdi[i] == '-') {
-				// Entre 0 et codePrecedent.get(i) - 1
-				int min = 0;
-				int max = codePrecedent.get(i);
-				int nombre = rd.nextInt(max - min) + min;
+				//enleve 1 au code precedent 
+				int nombre = codePrecedent.get(i);
+				nombre--;
 				code.add(nombre);
 			}
 		}

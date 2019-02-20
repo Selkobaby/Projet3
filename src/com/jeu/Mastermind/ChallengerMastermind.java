e3e99f2cf49a96d159fab63243db564513346c0a
Selami <bagcieselami@hotmail.fr>
2019-02-20 19:12:48 +0100

    ajout logger et properties

--- src/com/jeu/Mastermind/ChallengerMastermind.java
+++ src/com/jeu/Mastermind/ChallengerMastermind.java
@@ -1,15 +1,29 @@
-package com.jeu;
+package com.jeu.Mastermind;
 
 import java.util.ArrayList;
+import com.jeu.ClasseCreationJeux.Humain;
+import com.jeu.ClasseCreationJeux.Ordi;
+import com.jeu.ClasseCreationJeux.Utilitaire;
+import com.jeu.ClasseUtiliserRessources.EcrireJournal;
+import com.jeu.ClasseUtiliserRessources.Propriete;
 
-public class ChallengerMastermind{
+public class ChallengerMastermind extends EcrireJournal implements Propriete  {
 
 	public static void ChallengerM() {
 
-		// gnrer une combinaison alatoire
+		// generer une combinaison alatoire
 		ArrayList<Integer> codeAleatoire = Ordi.codeAleatoireMastermind();
-
-		int essaiMax = 10;
+		logger.info("L'ordi a saisi la combinaison que le joueur doit trouver.");
+		
+		int essaiMax = Integer.parseInt(properties.getProperty("essaiMax"));
+		logger.info("Chargement des propriétés : " + "nombre d'essais : " + essaiMax);
+		
+		if (properties.get("developerMode").equals("true")) {
+			System.out.print("La combinaison de l'ordi est : ");
+			for (int i = 0; i < codeAleatoire.size() ; i++) {
+				System.out.print(codeAleatoire.get(i));
+			}
+		}
 		
 		while (essaiMax > 0) {
 			
@@ -17,7 +31,9 @@ public class ChallengerMastermind{
 			System.out.println();
 			System.out.println("\nSaisir une combinaison a 4 chiffres entre 0 et 6.");
 			ArrayList<Integer> proposition = Humain.codeJoueur();
+			logger.info("Le joueur a saisi une combinaison a 4 chiffres.");
 	
+			//recherche des bien et mal place de la proposition du joueur
 			int bienPlace = Utilitaire.getBienPlace(codeAleatoire, proposition);
 			int malPlace = Utilitaire.getMalPlace(codeAleatoire, proposition);
 			
@@ -30,9 +46,11 @@ public class ChallengerMastermind{
 
 			//Le resultat de chaque comparaison
 			System.out.print(bienPlace + " bien place et " + malPlace + " mal place ");
-		
+			logger.info("Affichage du resultat de la combinaison du joueur.");
+			
 			if (bienPlace == codeAleatoire.size()) {
 				System.out.println("\nBravo vous avez gagne !");
+				logger.info("Le joueur a gagner");
 				essaiMax = 0;
 			}
 			essaiMax--;
@@ -40,7 +58,9 @@ public class ChallengerMastermind{
 			/* Si le nombre d'essai atteint 0, le jeu affiche 
 			la bonne combinaison qui indique que le joueur a perdu */
 			if (essaiMax == 0) {
+				logger.info("Le joueur a perdu");
 				System.out.println("Perdu, la combinaison de l'ordinateur etait : ");
+				logger.info("Affichage de la combinaison de l'ordi");
 				for (int i = 0; i < codeAleatoire.size(); i++) {
 					System.out.print(codeAleatoire.get(i));
 				}

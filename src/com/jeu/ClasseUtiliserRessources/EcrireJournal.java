6c7700b58250e1aa7e4a0ea1e23c71779e63b726
Selami <bagcieselami@hotmail.fr>
2019-02-20 19:09:36 +0100

    class abstraite pour utiliser logger

---
+++ src/com/jeu/ClasseUtiliserRessources/EcrireJournal.java
@@ -0,0 +1,9 @@
+package com.jeu.ClasseUtiliserRessources;
+
+import org.apache.log4j.Logger;
+
+public abstract class EcrireJournal {
+
+	protected static Logger logger = Logger.getLogger(Logger.class);
+	
+}

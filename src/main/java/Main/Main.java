33c88cb6ed4d66313d3e9da13133eb838815fe4b
h motors <hmotors@imac-de-h.home>
2019-02-22 15:59:25 +0100

    Main de l'application

---
+++ src/main/java/Main/Main.java
@@ -0,0 +1,87 @@
+package main.java.Main;
+
+import java.util.Properties;
+
+import org.apache.log4j.xml.DOMConfigurator;
+
+import main.java.ClasseCreationJeux.Choix;
+import main.java.ClasseCreationJeux.Message;
+import main.java.ClasseUtiliserRessources.EcrireJournal;
+import main.java.ClasseUtiliserRessources.LireFichierConfig;
+import main.java.ClasseUtiliserRessources.Propriete;
+import main.java.ClasseUtiliserRessources.SaveFichier;
+
+public class Main extends EcrireJournal implements Propriete {
+
+	public static void main(String[] args) {
+	
+		String fichierLog4j = System.getProperty("user.dir") + "\\" + "Projet3\\ressources\\res\\log4j.xml";
+		DOMConfigurator.configure(fichierLog4j);
+
+		new SaveFichier();
+		LireFichierConfig lireFichier = new LireFichierConfig();
+		Properties properties = lireFichier.getProperties();	
+		
+		if(args.length > 0) {
+			if(args[0].equals("true"))
+			{
+				properties.setProperty("developerMode", "true");
+			}
+		}
+		
+		String jeuRecherche = "Recherche+/-";
+		String jeuMastermind = "Mastermind";
+
+		char resultatChoix = ' ', jeu = ' ', modeJeu = ' ';
+
+		System.out.println();
+
+		do {
+			do {
+				jeu = Message.messageJeu();			
+
+				if (jeu != '1' && jeu != '2' && jeu != '3')
+					System.out.println("Vous devez choisir entre 1, 2, ou 3");
+					logger.info("Le joueur n'a pas choisi 1,2 ou 3 pour le jeu.");
+
+				if (jeu == '1') {
+					System.out.print("Vous avez choisi le jeu " + jeuRecherche + "\n");
+					logger.info("Le joueur a choisi le jeu de recherche.");
+					System.out.println();
+				} else if (jeu == '2') {
+					System.out.println("Vous avez choisi le jeu " + jeuMastermind + "\n");
+					logger.info("Le joueur a choisi le jeu de recherche.");
+					System.out.println();
+				} else if (jeu == '3') {
+					System.out.println("Fin du jeu");
+					logger.info("Le joueur a quitte le jeu.");
+					System.out.println();
+				}
+
+			} while (jeu != '1' && jeu != '2' && jeu != '3');
+
+			if (jeu == '1' || jeu == '2') {
+				do {
+					do {
+						modeJeu = Message.messageMode();
+
+						if (modeJeu != '1' && modeJeu != '2' && modeJeu != '3')
+							System.out.println("Vous devez choisir entre 1, 2 ou 3");
+							logger.info("Le joueur n'a pas choisi 1,2 ou 3 pour le mode de jeu.");
+
+					} while (modeJeu != '1' && modeJeu != '2' && modeJeu != '3');
+
+					Choix.getMode(jeu, modeJeu);
+					
+					do {
+						resultatChoix = Message.messageFin();
+						
+					} while (resultatChoix != 'o' && resultatChoix != 'n');
+
+				} while (resultatChoix == 'o');
+			}
+		} while (jeu != '3');
+
+		System.out.println("A bientot !");
+	}
+}

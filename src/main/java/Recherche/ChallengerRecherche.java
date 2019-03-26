f94fac250b31c5bda20945e4a3f6457429da4fed
Selami <bagcieselami@hotmail.fr>
2019-03-26 16:23:11 +0100

    reorganisation

--- src/main/java/Recherche/ChallengerRecherche.java
+++ src/main/java/Recherche/ChallengerRecherche.java
@@ -1,51 +1,50 @@
-package main.java.Recherche;
+package Recherche;
 
 import java.util.ArrayList;
 
-import main.java.ClasseCreationJeux.Humain;
-import main.java.ClasseCreationJeux.Ordi;
-import main.java.ClasseCreationJeux.Utilitaire;
-import main.java.ClasseUtiliserRessources.EcrireJournal;
-import main.java.ClasseUtiliserRessources.Propriete;
+import ClasseCreationJeux.Humain;
+import ClasseCreationJeux.Ordi;
+import ClasseCreationJeux.Utilitaire;
+import ClasseUtiliserRessources.EcrireJournal;
+import ClasseUtiliserRessources.Propriete;
 
-public class ChallengerRecherche extends EcrireJournal implements Propriete  {
-	
-	public static void ChallengerR(){
-		
-		// generer la combinaison aleatoire 
+public class ChallengerRecherche extends EcrireJournal implements Propriete {
+
+	public static void ChallengerR() {
+
+		// generer la combinaison aleatoire
 		ArrayList<Integer> codeAleatoire = Ordi.codeAleatoireR();
 		logger.info("L'ordi a saisi la combinaison que le joueur doit trouver.");
-		
+
 		int essaiMax = Integer.parseInt(properties.getProperty("essaiMax"));
 		logger.info("Chargement des proprietes : " + "nombre d'essais : " + essaiMax);
-		
+
 		if (properties.get("developerMode").equals("true")) {
 			System.out.print("La combinaison de l'ordi est : ");
-			for (int i = 0; i < codeAleatoire.size() ; i++) {
+			for (int i = 0; i < codeAleatoire.size(); i++) {
 				System.out.print(codeAleatoire.get(i));
 			}
 		}
-		
+
 		while (essaiMax > 0) {
 
-			
 			System.out.println("\nSaisir une combinaison a 4 chiffres entre 0 et 9");
 			ArrayList<Integer> proposition = Humain.codeJoueur();
 			logger.info("Le joueur a saisi une combinaison a 4 chiffres");
 
 			System.out.print("Proposition : ");
-			for (int i = 0; i < codeAleatoire.size() ; i++) {
+			for (int i = 0; i < codeAleatoire.size(); i++) {
 				System.out.print(proposition.get(i));
 			}
 			System.out.print("\nReponse : ");
-			
-			//recherche du resultat de la proposition
+
+			// recherche du resultat de la proposition
 			String resultat = Utilitaire.getResultatRechercher(codeAleatoire, proposition);
-			
-			//affichage du resultat
+
+			// affichage du resultat
 			System.out.println(resultat);
 			logger.info("Affichage du resultat de la combinaison.");
-			
+
 			// Afficher la victoire si la combinaison est ====
 			if (resultat.equals("====")) {
 				System.out.println("Bravo, vous avez gagne.");
@@ -53,8 +52,8 @@ public class ChallengerRecherche extends EcrireJournal implements Propriete  {
 				essaiMax = 0;
 			}
 			essaiMax--;
-			
-			//Afficher la combinaison avec les bon resultat
+
+			// Afficher la combinaison avec les bon resultat
 			if (essaiMax == 0) {
 				logger.info("Le joueur a perdu.");
 				System.out.print("Perdu, la combinaison de l'ordinateur etait : ");
@@ -64,5 +63,5 @@ public class ChallengerRecherche extends EcrireJournal implements Propriete  {
 				}
 			}
 		}
-	}		
+	}
 }

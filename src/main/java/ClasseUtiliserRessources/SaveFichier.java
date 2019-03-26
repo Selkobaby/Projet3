b3ac7877937f0b825a56b2913046d0de572535ae
Selami <bagcieselami@hotmail.fr>
2019-03-26 16:20:31 +0100

    reorganisation

--- src/main/java/ClasseUtiliserRessources/SaveFichier.java
+++ src/main/java/ClasseUtiliserRessources/SaveFichier.java
@@ -1,16 +1,17 @@
-package main.java.ClasseUtiliserRessources;
+package ClasseUtiliserRessources;
 
 import org.apache.log4j.xml.DOMConfigurator;
 
 public class SaveFichier {
-	
+
 	public void fichierJournal() {
-		
-		//recupere le repertoire ou le chemin de l'espace de travail pour le projet en cours
+
+		// recupere le repertoire ou le chemin de l'espace de travail pour le projet en
+		// cours
 		String sauvegarde = System.getProperty("user.dir") + "\\" + "Projet3\\src\\ressources\\journal.txt";
-		//configure sauvegarde tel que defini dans le journal log4j.dtd.
+		// configure sauvegarde tel que defini dans le journal log4j.dtd.
 		DOMConfigurator.configure(sauvegarde);
-		
+
 	}
 
 }

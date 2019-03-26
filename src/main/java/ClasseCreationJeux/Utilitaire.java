1e04b38ef2c5a3e25096f00e62d512a4aebca9cd
Selami <bagcieselami@hotmail.fr>
2019-03-26 16:18:40 +0100

    reorganisation

--- src/main/java/ClasseCreationJeux/Utilitaire.java
+++ src/main/java/ClasseCreationJeux/Utilitaire.java
@@ -1,14 +1,16 @@
-package main.java.ClasseCreationJeux;
+package ClasseCreationJeux;
 
 import java.util.ArrayList;
 
 public class Utilitaire {
-	/* comparaison entre le code propose et le code cherche pour 
-	indique les bien place*/
-	public static int getBienPlace(ArrayList<Integer> codeChercher ,ArrayList<Integer> codeProposer) {
-		
-		int bienPlace = 0; 
-		
+	/*
+	 * comparaison entre le code propose et le code cherche pour indique les bien
+	 * place
+	 */
+	public static int getBienPlace(ArrayList<Integer> codeChercher, ArrayList<Integer> codeProposer) {
+
+		int bienPlace = 0;
+
 		for (int i = 0; i < codeChercher.size(); i++) {
 			int nombre = codeProposer.get(i);
 			if (codeChercher.get(i) == nombre) {
@@ -17,25 +19,27 @@ public class Utilitaire {
 		}
 		return bienPlace;
 	}
-	
-	/* comparaison entre le code propose et le code cherche pour 
-	indique les mal place*/
-	public static int getMalPlace(ArrayList<Integer> codeChercher ,ArrayList<Integer> codeProposer) {
+
+	/*
+	 * comparaison entre le code propose et le code cherche pour indique les mal
+	 * place
+	 */
+	public static int getMalPlace(ArrayList<Integer> codeChercher, ArrayList<Integer> codeProposer) {
 		int nbreMalPlace = 0;
-		int malPlace = 0; 
-		
+		int malPlace = 0;
+
 		for (int i = 0; i < codeChercher.size(); i++) {
 			int nombre = codeProposer.get(i);
 			if (codeChercher.contains(nombre)) {
 				malPlace++;
 			}
-		nbreMalPlace = malPlace - getBienPlace(codeChercher, codeProposer);
+			nbreMalPlace = malPlace - getBienPlace(codeChercher, codeProposer);
 		}
 		return nbreMalPlace;
 	}
-	
+
 	// Comparer les 2 code pour avoir un resultat +, - ou =
-	public static String getResultatRechercher(ArrayList<Integer> codeChercher ,ArrayList<Integer> codeProposer) {
+	public static String getResultatRechercher(ArrayList<Integer> codeChercher, ArrayList<Integer> codeProposer) {
 		String resultat = "";
 
 		for (int i = 0; i < codeChercher.size(); i++) {

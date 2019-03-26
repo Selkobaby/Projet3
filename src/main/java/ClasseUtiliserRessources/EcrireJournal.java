fccb1a3e26d66447f553660c364359d9883c0a7f
Selami <bagcieselami@hotmail.fr>
2019-03-26 16:19:32 +0100

    reorganisation

--- src/main/java/ClasseUtiliserRessources/EcrireJournal.java
+++ src/main/java/ClasseUtiliserRessources/EcrireJournal.java
@@ -1,9 +1,9 @@
-package main.java.ClasseUtiliserRessources;
+package ClasseUtiliserRessources;
 
 import org.apache.log4j.Logger;
 
 public abstract class EcrireJournal {
 
 	protected static Logger logger = Logger.getLogger(Logger.class);
-	
+
 }

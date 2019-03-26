290dad7875b89de558cd90430942cb6cd3f18e16
Selami <bagcieselami@hotmail.fr>
2019-03-26 16:23:32 +0100

    reorganisation

--- src/main/java/Recherche/DefenseurRecherche.java
+++ src/main/java/Recherche/DefenseurRecherche.java
@@ -1,12 +1,12 @@
-package main.java.Recherche;
+package Recherche;
 
 import java.util.ArrayList;
 
-import main.java.ClasseCreationJeux.Humain;
-import main.java.ClasseCreationJeux.Ordi;
-import main.java.ClasseCreationJeux.Utilitaire;
-import main.java.ClasseUtiliserRessources.EcrireJournal;
-import main.java.ClasseUtiliserRessources.Propriete;
+import ClasseCreationJeux.Humain;
+import ClasseCreationJeux.Ordi;
+import ClasseCreationJeux.Utilitaire;
+import ClasseUtiliserRessources.EcrireJournal;
+import ClasseUtiliserRessources.Propriete;
 
 public class DefenseurRecherche extends EcrireJournal implements Propriete {
 
@@ -16,10 +16,10 @@ public class DefenseurRecherche extends EcrireJournal implements Propriete {
 		System.out.println("\nSaisir une combinaison a 4 chiffres entre 0 et 9 ");
 		ArrayList<Integer> codeJoueur = Humain.codeJoueur();
 		logger.info("Le joueur a saisi la combinaison que l'ordi doit trouver.");
-		
+
 		int essaiMax = Integer.parseInt(properties.getProperty("essaiMax"));
 		logger.info("Chargement des proprietes : " + "nombre d'essais : " + essaiMax);
-		
+
 		ArrayList<Integer> propositionOrdi = null;
 		String resultat = null;
 
@@ -27,34 +27,34 @@ public class DefenseurRecherche extends EcrireJournal implements Propriete {
 
 			// Faire une proposition
 			propositionOrdi = Ordi.codeAleatoireRecherche(propositionOrdi, resultat);
-			if(propositionOrdi == null) {
+			if (propositionOrdi == null) {
 				logger.info("L'ordi a saisi une proposition.");
-			}else {
+			} else {
 				logger.info("L'ordi a saisi une nouvelle proposition.");
 			}
-		
-			// Compare les 2 combinaisons 
+
+			// Compare les 2 combinaisons
 			resultat = Utilitaire.getResultatRechercher(codeJoueur, propositionOrdi);
-			
+
 			System.out.print("Proposition : ");
 			for (int j = 0; j < codeJoueur.size(); j++) {
 				System.out.print(propositionOrdi.get(j));
 			}
-			
+
 			System.out.print(" -> reponse : ");
 			System.out.println(resultat);
-			
+
 			// Afficher la victoire si la combinaison est ====
 			if (resultat.equals("====")) {
 				System.out.println("Bravo, vous avez gagne");
 				essaiMax = 0;
 			}
 			essaiMax--;
-			
-			//si le nombre maximum d'essai est atteint l'ordinateur a perdu
+
+			// si le nombre maximum d'essai est atteint l'ordinateur a perdu
 			if (essaiMax == 0) {
 				System.out.print("Perdu, la combinaison du joueur etait ");
-				for (int i = 0; i <codeJoueur.size() ; i++) {
+				for (int i = 0; i < codeJoueur.size(); i++) {
 					System.out.print(codeJoueur.get(i));
 				}
 				System.out.println(".");

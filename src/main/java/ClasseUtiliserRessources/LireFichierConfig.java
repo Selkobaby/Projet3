d717dfe93b21ce277872bd1b94d69764be052b44
Selami <bagcieselami@hotmail.fr>
2019-03-26 16:19:52 +0100

    reorganisation

--- src/main/java/ClasseUtiliserRessources/LireFichierConfig.java
+++ src/main/java/ClasseUtiliserRessources/LireFichierConfig.java
@@ -1,22 +1,24 @@
-package main.java.ClasseUtiliserRessources;
+package ClasseUtiliserRessources;
 
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.Properties;
 
 public class LireFichierConfig {
-	
+
 	public Properties getProperties() {
-		
+
 		Properties properties = new Properties();
-		
+
 		try {
 			String fichierConfig = System.getProperty("user.dir") + "//" + "src//main//ressources//config.properties";
-			//Cree un FileInputStreamen ouvrant une connexion e un fichier reel, le fichier nomme par le nom du chemin d'acces name dans le systeme de fichiers.
+			// Cree un FileInputStreamen ouvrant une connexion e un fichier reel, le fichier
+			// nomme par le nom du chemin d'acces name dans le systeme de fichiers.
 			FileInputStream in = new FileInputStream(fichierConfig);
-			//Lit une liste de proprietes a partir du flux d'octets en entree.
+			// Lit une liste de proprietes a partir du flux d'octets en entree.
 			properties.load(in);
-			//Ferme ce flux d'entree de fichier et libere toutes les ressources systeme associees au flux.
+			// Ferme ce flux d'entree de fichier et libere toutes les ressources systeme
+			// associees au flux.
 			in.close();
 		} catch (IOException e) {
 			System.out.println("Erreur de chargement du fichier de configuration !");

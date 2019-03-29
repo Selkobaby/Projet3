e060385af729461b5559f878dae2735862739d36
bagci <bagciselami@hotmail.fr>
2019-03-29 08:33:35 +0100

    ajout properties et lire fichier

--- src/main/java/ClasseCreationJeux/Humain.java
+++ src/main/java/ClasseCreationJeux/Humain.java
@@ -1,17 +1,21 @@
 package main.java.ClasseCreationJeux;
 
 import java.util.ArrayList;
+import java.util.Properties;
 import java.util.Scanner;
 
 import main.java.ClasseUtiliserRessources.EcrireJournal;
-import main.java.ClasseUtiliserRessources.Propriete;
+import main.java.ClasseUtiliserRessources.LireFichierConfig;
 
-public class Humain extends EcrireJournal implements Propriete {
+public class Humain extends EcrireJournal{
 
 	/*
 	 * codeJoueur permet de recuperer le code e 4 chiffres que va saisir le Joueur
 	 */
 	public static ArrayList<Integer> codeJoueur() {
+		
+		LireFichierConfig lire = new LireFichierConfig();
+		Properties properties = lire.getProperties();
 
 		int nbreChiffresCombi = Integer.parseInt(properties.getProperty("nombreChiffresCombi"));
 		@SuppressWarnings("resource")

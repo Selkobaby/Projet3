3eeae895d39d54bc97a130000c6101de60bb8489
bagci <bagciselami@hotmail.fr>
2019-03-29 08:19:50 +0100

    ajout properties

--- src/main/java/ClasseCreationJeux/Choix.java
+++ src/main/java/ClasseCreationJeux/Choix.java
@@ -1,6 +1,7 @@
 package main.java.ClasseCreationJeux;
 
 import main.java.ClasseUtiliserRessources.EcrireJournal;
+import main.java.ClasseUtiliserRessources.Propriete;
 import main.java.Mastermind.ChallengerMastermind;
 import main.java.Mastermind.DefenseurMastermind;
 import main.java.Mastermind.DuelMastermind;
@@ -8,10 +9,11 @@ import main.java.Recherche.ChallengerRecherche;
 import main.java.Recherche.DefenseurRecherche;
 import main.java.Recherche.DuelRecherche;
 
-public class Choix extends EcrireJournal {
+public class Choix extends EcrireJournal{
 
-	public static void getMode(char jeu, char modeJeu) {
+	public static Propriete get(char jeu, char modeJeu){
 
+		Propriete propriete = null;
 		String choixChallenger = "Mode challenger";
 		String choixDefenseur = "Mode defenseur";
 		String choixDuel = "Mode duel";
@@ -20,34 +22,35 @@ public class Choix extends EcrireJournal {
 			System.out.println("Vous avez choisi le " + choixChallenger);
 			System.out.println("Vous devez trouver la combinaison secrete de l'ordinateur.");
 			logger.info("Le joueur a choisi le mode challenger du jeu recherche.");
-			ChallengerRecherche.ChallengerR();
+			propriete = new ChallengerRecherche();
 		} else if (jeu == '1' && modeJeu == '2') {
 			System.out.println("Vous avez choisi le " + choixDefenseur);
 			System.out.println("C'est a l'ordinateur de trouver votre combinaison secrete.");
 			logger.info("Le joueur a choisi le mode defenseur du jeu recherche.");
-			DefenseurRecherche.DefenseurR();
+			propriete = new DefenseurRecherche();
 		} else if (jeu == '1' && modeJeu == '3') {
 			System.out.println("Vous avez choisi le " + choixDuel);
 			System.out.println(
 					"L'ordinateur et vous jouez tour a tour, le premier a trouver la combinaison secrete de l'autre a gagne.");
 			logger.info("Le joueur a choisi le mode duel du jeu recherche.");
-			DuelRecherche.DuelR();
+			propriete = new DuelRecherche();
 		} else if (jeu == '2' && modeJeu == '1') {
 			System.out.println("Vous avez choisi le " + choixChallenger);
 			System.out.println("Vous devez trouver la combinaison secrete de l'ordinateur.");
 			logger.info("Le joueur a choisi le mode challenger du jeu mastermind.");
-			ChallengerMastermind.ChallengerM();
+			propriete = new ChallengerMastermind();
 		} else if (jeu == '2' && modeJeu == '2') {
 			System.out.println("Vous avez choisi le " + choixDefenseur);
 			System.out.println("C'est a l'ordinateur de trouver votre combinaison secrete.");
 			logger.info("Le joueur a choisi le mode defenseur du jeu mastermind.");
-			DefenseurMastermind.DefenseurM();
+			propriete = new DefenseurMastermind();
 		} else if (jeu == '2' && modeJeu == '3') {
 			System.out.println("Vous avez choisi le " + choixDuel);
 			System.out.println(
 					"L'ordinateur et vous jouez tour a tour, le premier a trouver la combinaison secrete de l'autre a gagne.");
 			logger.info("Le joueur a choisi le mode duel du jeu mastermind.");
-			DuelMastermind.DuelM();
+			propriete = new DuelMastermind();
 		}
-	}
+	return propriete;
+	}	
 }

4d9c6fd9546ce7238c6472df36b2d4c374472313
bagci <bagciselami@hotmail.fr>
2019-04-10 18:43:09 +0200

    modif chemin

--- src/main/java/ClasseUtiliserRessources/SaveFichier.java
+++ src/main/java/ClasseUtiliserRessources/SaveFichier.java
@@ -8,7 +8,7 @@ public class SaveFichier {
 
 		// recupere le repertoire ou le chemin de l'espace de travail pour le projet en
 		// cours
-		String sauvegarde = System.getProperty("user.dir") + "\\" + "Projet3\\src\\main\\ressources\\journal.txt";
+		String sauvegarde = System.getProperty("user.dir") + "\\" + "main\ressources\\journal.txt";
 		// configure sauvegarde tel que defini dans le journal log4j.dtd.
 		DOMConfigurator.configure(sauvegarde);
 

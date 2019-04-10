9f51b18a5061d0c0db429ef57f96c1c2eaff102d
bagci <bagciselami@hotmail.fr>
2019-04-10 18:45:57 +0200

    ajout logger

--- src/main/java/Mastermind/DefenseurMastermind.java
+++ src/main/java/Mastermind/DefenseurMastermind.java
@@ -3,13 +3,16 @@ package main.java.Mastermind;
 import java.util.ArrayList;
 import java.util.Properties;
 
+import org.apache.log4j.Logger;
+
 import main.java.ClasseCreationJeux.Humain;
 import main.java.ClasseCreationJeux.Ordi;
 import main.java.ClasseCreationJeux.Utilitaire;
-import main.java.ClasseUtiliserRessources.EcrireJournal;
 import main.java.ClasseUtiliserRessources.Propriete;
 
-public class DefenseurMastermind extends EcrireJournal implements Propriete {
+public class DefenseurMastermind implements Propriete {
+	
+	private static Logger logger = Logger.getLogger(Logger.class);
 
 	public void run(Properties properties) {
 
